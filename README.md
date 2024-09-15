2f0ab11e20d7cd7a20208e04da03115793dbbdb4
kimYunHyeong <90606145+kimYunHyeong@users.noreply.github.com>
2024-09-15 22:58:23 +0900

    understanding router

---
+++ README.md
@@ -0,0 +1,15 @@
+#Wetube-clone
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch video
+/videos/edit -> edit video
+/videos/delete -> Delete video
+/videos/comments -> comments on a video
+/videos/comments/delete -> delete comments of a video
--- src/server.js
+++ src/server.js
@@ -1,37 +1,20 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("NOT ALLOWED");
-  }
-  console.log("Allowed. You may continue");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.end();
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge");
-};
-
-app.use(logger); //글로벌한 미들웨어를 만들어줌. 어떠한 유알엘에서도 사용 가능
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 app.listen(
   PORT,
-  console.log(`Server listening on port http://localhost:${PORT}`)
+  console.log(`Server l istening on port http://localhost:${PORT}`)
 );
