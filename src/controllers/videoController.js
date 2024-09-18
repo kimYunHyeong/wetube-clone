0a2eacc5ac8318696cd989602d9e963b68418a27
kimYunHyeong <90606145+kimYunHyeong@users.noreply.github.com>
2024-09-18 15:52:26 +0900

    install pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,12 @@
-export const showHomepage = (req, res) => res.send("Home");
-export const search = (res, req) => res.send("search");
-export const see = (req, res) => res.send("Watch Videos");
-export const edit = (req, res) => res.send("Edit Videos");
-export const deleteVideo = (req, res) => res.send("delte Videos");
+export const showHomepage = (req, res) => res.render("home");
+export const search = (res, req) => res.render("see");
+export const see = (req, res) => res.render("see");
+export const edit = (req, res) => {
+  console.log(req.params);
+  return res.send("Edit Videos");
+};
+export const deleteVideo = (req, res) => {
+  console.log(req.params);
+  return res.send("delte Videos");
+};
 export const upload = (req, res) => res.send("upload Videos");
