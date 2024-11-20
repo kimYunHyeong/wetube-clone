9bfd016969fd37b633ad95b0c1505d43b4e53b3d
kimYunHyeong <90606145+kimYunHyeong@users.noreply.github.com>
2024-11-21 01:51:45 +0900

    delete fake db and connect to mongo DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,78 +1,40 @@
+import Video from "../models/Video";
+
 const faker = {
   username: "Kim Yun Hyeong",
   loggedIn: false,
 };
 
-let videos = [
-  {
-    title: "hello1",
-    rating: 5,
-    comments: 2,
-    cratedAt: "2 minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "hello2",
-    rating: 5,
-    comments: 2,
-    cratedAt: "2 minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "hello3",
-    rating: 5,
-    comments: 2,
-    cratedAt: "2 minutes ago",
-    views: 59,
-    id: 3,
-  },
-  {
-    title: "hello4",
-    rating: 5,
-    comments: 2,
-    cratedAt: "2 minutes ago",
-    views: 59,
-    id: 4,
-  },
-  {
-    title: "hello5",
-    rating: 5,
-    comments: 2,
-    cratedAt: "2 minutes ago",
-    views: 59,
-    id: 5,
-  },
-];
-
-export const showHomepage = (req, res) =>
-  res.render("home", { pageTitle: "Home", videos });
+export const showHomepage = (req, res) => {
+  Video.find({})
+    .then((videos) => {
+      console.log("videos", videos);
+      return res.render("home", { pageTitle: "Home", videos: videos });
+    })
+    .catch((error) => {
+      console.log("errors", error);
+    });
+  //res.render("home", { pageTitle: "Home", videos: [] });
+};
 
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
 
   res.render("watch", {
     pageTitle: `watching ${video.title}`,
-
-    video,
   });
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
   res.render("edit", {
     pageTitle: `editing ${video.title}`,
-    video,
   });
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -83,14 +45,5 @@ export const getUpload = (req, res) => {
 export const postUpload = (req, res) => {
   //add video
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 5,
-    comments: 2,
-    cratedAt: "2 minutes ago",
-    views: 59,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   res.redirect("/");
 };
